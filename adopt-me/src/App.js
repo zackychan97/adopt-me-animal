ab1cd89a02edf618c0d90bbb7d0caf7c1d035852
Zack Bumpous <upwork.ztb@gmail.com>
2020-04-30 18:58:05 -0500

    Generates a package.json file, sets up the Prettier extension so that it will only work inside this project unless set up in a diff project, went over the npm Scripts, set up ESLint and configures it, adds .gitignore and the appropriate things to ignore. Also npm installs a dev dependency called Parcel.

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,27 +1,26 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ])
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 };
-
-
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "Milo", animal: "Dog", breed: "Jack Russel"}),
-            React.createElement(Pet, { name: "Diesel", animal: "Dog", breed: "Lab"}),
-            React.createElement(Pet, { name: "Goldy", animal: "Dog", breed: "Lab/Poodle"})
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Milo",
+      animal: "Dog",
+      breed: "Jack Russel",
+    }),
+    React.createElement(Pet, { name: "Diesel", animal: "Dog", breed: "Lab" }),
+    React.createElement(Pet, {
+      name: "Goldy",
+      animal: "Dog",
+      breed: "Lab/Poodle",
+    }),
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
