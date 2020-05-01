fe58bfe41888dc53d86b7b3d834d105685baa93c
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-01 12:07:34 -0500

    Creates another useState hook, this time setting up which animal we would like to search for on our search params form, also imports ANIMALS from @frontendmasters/pet and incorporates that into our animal options. So people do not have to type which animal they want and that we have access to in our API, they can always just look and see what the options are.

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); // this is a react hook -- all hooks begin with use -- useState, useEffect, useMemo, etc.
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)} // takes an event,
           />
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
