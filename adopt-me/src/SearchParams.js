d7897f3ec5f11aa59ae2ad320441a03cd633d52f
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-02 19:29:38 -0500

    Adds a custom hook called useDropdown so we sort of are reusing a lot of logic for the animal dropdown and the breed dropdown, although we are not yet getting data from the Breed API.

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown.js";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); // this is a react hook -- all hooks begin with use -- useState, useEffect, useMemo, etc.
-  const [animal, setAnimal] = useState("dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -19,39 +20,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)} // takes an event,
           />
         </label>
-        <label htmlFor="animal">
-          animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>All</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
