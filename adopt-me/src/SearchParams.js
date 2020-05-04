16e317fed0191faaead069d78a529abb96f3d6a7
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-03 21:10:55 -0500

    Imports setBreed from useDropdown. Adds a useEffect that will setBreeds back to an empty array bc we want to rid of existing breeds inside of this array when we change breeds. Also adds setBreed inside of our useEffect that will setBreed to an empty string for similar purposes. Lastly inside our useEffect we map through the breeds and set them to strings.

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown.js";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); // this is a react hook -- all hooks begin with use -- useState, useEffect, useMemo, etc.
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]); // Whenever this runs we want to update breeds w/ an empty array. Cause if we're requesting new breeds and there are already breeds we want them to go away.
+    setBreed(""); // Whenever this runs it updates the text to an empty string so we don't have labrador cats
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }); // EVERYTHING else gets rendered first before a useEffect. Helps speed of loading website. It is asynchronous.
 
   return (
     <div className="search-params">
