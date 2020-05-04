29a74044a065b7471e2a17022f8c4f6e34e37692
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-04 16:01:48 -0500

    Creates a Details.js component, npm installs reach/router and sets up the router, so SearchParams becomes our default / path and Details becomes /details/:id

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-// import Pet from "./Pet";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams.js";
+import Details from "./Details.js";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
