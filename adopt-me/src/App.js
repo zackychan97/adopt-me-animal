3f7a25a525417c7593d6e073dbbd428d11fb65cb
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-07 19:47:10 -0500

    Adds Link from reach/router inside app by import, and then wraps our Adopt Me logo with that Link, taking us back to the applications home.

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams.js";
 import Details from "./Details.js";
 
@@ -8,7 +8,11 @@ const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
+        <header>
+          <Link to="/">
+            Adopt Me!
+          </Link>
+        </header>
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
